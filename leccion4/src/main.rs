ec6e0075625e7a28f96e8e1b0a8b30e7f48d26d6
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-10 15:15:06 -0300

    metodo match, if else, operadores logicos y relacionales

--- leccion4/src/main.rs
+++ leccion4/src/main.rs
@@ -5,7 +5,44 @@ fn main() {
     println!("{}", 8 > 3 && 6 == 6); //AMBAS DEBEN SER TRUE PARA QUE DEVUELVA TRUE (&&)
     println!("{}", 8 > 3 || 1 > 3); //una de las expresiones true es suficiente
     println!("{}", !true); //este lo convierte en false
-}
-//ALT 124 para obtener lineas verticales pipe
 
-//CONDICIONALES
+    //ALT 124 para obtener lineas verticales pipe
+
+    //CONDICIONALES
+    let maximo = 50;
+    let diminuto = 5;
+    let size = 1;
+    if size <= maximo {
+        println!("{} es pequeño en comparacion a {}", size, maximo);
+    } else {
+        println!("es grande");
+    }
+
+    if size <= diminuto {
+        println!("{} es diminuto con respecto a {}", size, maximo);
+    } else if size > diminuto && size < maximo {
+        println!("{} es mediano", size);
+    } else if size <= 0 {
+        println!("es cero o negativo");
+    } else {
+        println!("{} es enorme y sobrepasa a {}", size, maximo);
+    }
+    println!("********************************************************");
+
+    //trabajando con declaraciones coincidentes
+    //METODO MATCH
+    //se asemeja a la estructura "switch"
+
+    let codigo_trabajo = "V";
+
+    let puesto = match codigo_trabajo {
+        "M" => {
+            println!("MARKETING");
+            "MARKETING"
+        }
+        "C" => "CONTABILIDAD",
+        "V" => "VENTAS",
+        _ => "OTROS",
+    };
+    println!("area: {}", puesto);
+}
